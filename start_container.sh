80296ea246a967a4318c26b1a35a3aa8fc1a211a
amar0898 <amar.pinjore0898@gmail.com>
2025-01-14 18:17:46 -0500

    Updated Start_Container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull amar0898/aws-ci-cd-react-app:latest
+docker pull amar0898/aws-ci-cd-react-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 80:80 amar0898/aws-ci-cd-react-app:latest
